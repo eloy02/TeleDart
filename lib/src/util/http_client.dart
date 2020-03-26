770238164c06f766e1e9bb97fb2271b422eb8883
eloy02 <tis.str02@gmail.com>
2020-03-26 17:04:20 +0500

    added dio as http_client

--- lib/src/util/http_client.dart
+++ lib/src/util/http_client.dart
@@ -19,52 +19,37 @@
 import 'dart:async';
 import 'dart:convert';
 
-import 'package:http/http.dart' as http;
+import 'package:dio/adapter.dart';
+import 'package:dio/dio.dart';
 
 class HttpClient {
   /// HTTP get method
   /// [url] request url with query string (required)
-  Future<dynamic> httpGet(String url) async => http.get(url).then((response) {
-        Map<String, dynamic> body = jsonDecode(response.body);
-        if (body['ok']) {
-          return body['result'];
-        } else {
-          return Future.error(HttpClientException(
-              '${body['error_code']} ${body['description']}'));
-        }
-      }).catchError((error) => Future.error(HttpClientException('${error}')));
+  Future<dynamic> httpGet(String url, {String proxy}) async {
+    var dio = _initializeClient(proxy: proxy);
+
+    return dio.get(url).then((response) {
+      Map<String, dynamic> body = jsonDecode(response.data);
+      if (body['ok']) {
+        return body['result'];
+      } else {
+        return Future.error(HttpClientException(
+            '${body['error_code']} ${body['description']}'));
+      }
+    }).catchError((error) => Future.error(HttpClientException('${error}')));
+  }
 
   /// HTTP post method (x-www-form-urlencoded)
   /// [url] - request url (required)
   /// [body] - parameters in map
-  Future<dynamic> httpPost(String url, {Map<String, dynamic> body}) async =>
-      http
-          .post(url, body: body.map((k, v) => MapEntry(k, '${v}')))
-          .then((response) {
-        Map<String, dynamic> responseBody = jsonDecode(response.body);
-        if (responseBody['ok']) {
-          return responseBody['result'];
-        } else {
-          return Future.error(HttpClientException(
-              '${responseBody['error_code']} ${responseBody['description']}'));
-        }
-      }).catchError((error) => Future.error(HttpClientException('${error}')));
+  Future<dynamic> httpPost(String url,
+      {String proxy, Map<String, dynamic> body}) async {
+    var dio = _initializeClient(proxy: proxy);
 
-  /// HTTP post method (multipart/form-data)
-  /// [url] - request url (required)
-  /// [file] - file to upload (required)
-  /// [body] - parameters in map
-  Future<dynamic> httpMultipartPost(String url, List<http.MultipartFile> files,
-      {Map<String, dynamic> body}) async {
-    var request = http.MultipartRequest('POST', Uri.parse(url))
-      ..headers.addAll({'Content-Type': 'multipart/form-data'})
-      ..fields.addAll(body.map((k, v) => MapEntry(k, '${v}')))
-      ..files.addAll(files);
-    return request
-        .send()
-        .then((response) => http.Response.fromStream(response))
+    return dio
+        .post(url, data: body.map((k, v) => MapEntry(k, '${v}')))
         .then((response) {
-      Map<String, dynamic> responseBody = jsonDecode(response.body);
+      Map<String, dynamic> responseBody = jsonDecode(response.data);
       if (responseBody['ok']) {
         return responseBody['result'];
       } else {
@@ -72,6 +57,62 @@ class HttpClient {
             '${responseBody['error_code']} ${responseBody['description']}'));
       }
     }).catchError((error) => Future.error(HttpClientException('${error}')));
+  }
+
+  /// HTTP post method (multipart/form-data)
+  /// [url] - request url (required)
+  /// [file] - file to upload (required)
+  /// [body] - parameters in map
+  Future<dynamic> httpMultipartPost(String url, List<MultipartFile> files,
+      {Map<String, dynamic> body, String proxy}) async {
+    var dio = _initializeClient(
+        proxy: proxy /*, headers: {'Content-Type': 'multipart/form-data'}*/);
+
+    var data = FormData.fromMap(body.map((k, v) => MapEntry(k, '${v}'))
+      ..addAll({
+        'files': [files]
+      }));
+
+    return dio.post(url, data: data).then((response) {
+      Map<String, dynamic> responseBody = jsonDecode(response.data);
+      if (responseBody['ok']) {
+        return responseBody['result'];
+      } else {
+        return Future.error(HttpClientException(
+            '${responseBody['error_code']} ${responseBody['description']}'));
+      }
+    }).catchError((error) => Future.error(HttpClientException('${error}')));
+  }
+
+  Future<dynamic> httpFormDataPost(String url, FormData formData,
+      {String proxy}) {
+    var dio = _initializeClient(proxy: proxy);
+    return dio.post(url, data: formData).then((response) {
+      Map<String, dynamic> responseBody = jsonDecode(response.data);
+      if (responseBody['ok']) {
+        return responseBody['result'];
+      } else {
+        return Future.error(HttpClientException(
+            '${responseBody['error_code']} ${responseBody['description']}'));
+      }
+    }).catchError((error) => Future.error(HttpClientException('${error}')));
+  }
+
+  Dio _initializeClient({String proxy, Map<String, dynamic> headers}) {
+    var dio = Dio();
+
+    if (proxy.isNotEmpty) {
+      (dio.httpClientAdapter as DefaultHttpClientAdapter).onHttpClientCreate =
+          (client) {
+        client.findProxy = (uri) => proxy;
+      };
+    }
+
+    if (headers.isNotEmpty) {
+      dio.options.headers.addAll(headers);
+    }
+
+    return dio;
   }
 }
 
